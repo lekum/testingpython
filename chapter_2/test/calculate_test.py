ae39432fc5eb34241ea3f244fb7218c63cd72a57
Alejandro Guirao <lekumberri@gmail.com>
2014-11-19 10:39:28 +0100

    Fix typo

--- chapter_2/test/calculate_test.py
+++ chapter_2/test/calculate_test.py
@@ -9,7 +9,7 @@ class TestCalculate(unittest.TestCase):
     def test_add_method_retuns_correct_result(self):
         self.assertEqual(4, self.calc.add(2, 2))
 
-    def test_add_method_raises_execption_with_strings(self):
+    def test_add_method_raises_exception_with_strings(self):
         self.assertRaises(TypeError, self.calc.add, "Hello", "World")
 
 if __name__ == '__main__':
